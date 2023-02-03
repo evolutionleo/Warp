b0321f1a10c10143a776085f3b1ec41cec89c4ad
Evoleo <44549380+evolutionleo@users.noreply.github.com>
2023-02-03 17:08:35 +0300

    Friend requests fix

--- TypescriptServer/src/internal/schemas/friend_request.ts
+++ TypescriptServer/src/internal/schemas/friend_request.ts
@@ -9,45 +9,45 @@ import { Account, IAccount } from '#schemas/account';
 import Profile, { IProfile } from '#schemas/profile';
 
 export interface IFriendRequest extends Document {
-    sender_id: ObjectId;
-    receiver_id: ObjectId;
+    sender: ObjectId;
+    receiver: ObjectId;
 }
 
 export interface IFriendRequestModel extends Model<IFriendRequest> {
     findIncoming(profile_id:string):Promise<IProfile[]>;
     findOutgoing(profile_id:string):Promise<IProfile[]>;
-    findRequestId(sender_id:string, receiver_id:string):Promise<ObjectId>;
-    requestExists(sender_id:string, receiver_id:string):Promise<boolean>;
+    findRequestId(sender:string, receiver:string):Promise<ObjectId>;
+    requestExists(sender:string, receiver:string):Promise<boolean>;
     accept(req_id:ObjectId|string):Promise<void>;
     reject(req_id:ObjectId|string):Promise<void>;
     cancel(req_id:ObjectId|string):Promise<void>;
 }
 
 const friendRequestSchema = new Schema({
-    sender_id: { type: Schema.Types.ObjectId, ref: 'Profile' },
-    receiver_id: { type: Schema.Types.ObjectId, ref: 'Profile' }
+    sender: { type: Schema.Types.ObjectId, ref: 'Profile' },
+    receiver: { type: Schema.Types.ObjectId, ref: 'Profile' }
 }, {collection: 'FriendRequests'});
 
-friendRequestSchema.statics.findAllIncoming = async function(profile_id:string):Promise<IProfile[]> {
-    return await FriendRequest.find({ receiver: profile_id }).populate('sender');
+friendRequestSchema.statics.findIncoming = async function(profile_id:string):Promise<IProfile[]> {
+    return await (await FriendRequest.find({ receiver: profile_id }).populate<{ sender: IProfile }>('sender')).map(req => req.sender);
 }
 
-friendRequestSchema.statics.findAllOutgoing = async function(profile_id:string):Promise<IProfile[]> {
-    return await FriendRequest.find({ sender: profile_id }).populate('sender');
+friendRequestSchema.statics.findOutgoing = async function(profile_id:string):Promise<IProfile[]> {
+    return (await FriendRequest.find({ sender: profile_id }).populate<{ receiver: IProfile }>('receiver')).map(req => req.receiver);
 }
 
-friendRequestSchema.statics.findRequestId = async function(sender_id:string, receiver_id:string):Promise<IFriendRequest> {
-    return (await FriendRequest.exists({ sender_id, receiver_id }))._id;
+friendRequestSchema.statics.findRequestId = async function(sender:string, receiver:string):Promise<IFriendRequest> {
+    return (await FriendRequest.exists({ sender, receiver }))._id;
 }
 
-friendRequestSchema.statics.requestExists = async function(sender_id:string, receiver_id:string):Promise<boolean> {
-    return !!await FriendRequest.exists({ sender_id, receiver_id });
+friendRequestSchema.statics.requestExists = async function(sender:string, receiver:string):Promise<boolean> {
+    return !!await FriendRequest.exists({ sender, receiver });
 }
 
 friendRequestSchema.statics.accept = async function(_id:ObjectId|string):Promise<void> {
     let req:IFriendRequest = await FriendRequest.findById(_id);
-    await Profile.findByIdAndUpdate(req.sender_id, { $push: { friends: req.receiver_id } });
-    await Profile.findByIdAndUpdate(req.receiver_id, { $push: { friends: req.sender_id  } });
+    await Profile.findByIdAndUpdate(req.sender, { $push: { friends: req.receiver } });
+    await Profile.findByIdAndUpdate(req.receiver, { $push: { friends: req.sender  } });
     await req.deleteOne();
 }
 
